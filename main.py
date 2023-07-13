2dd08803900a6bcfc532e8abf3891f639f619161
you2zz <yri_vla@mail.ru>
2023-07-13 13:52:09 +0500

    учтены замечания эксперта

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-from models import create_tables, Publisher, Stock, Book, Shop, Sale
+from models import get_sales, create_tables, Publisher, Stock, Book, Shop, Sale
 
 load_dotenv()
 
@@ -39,15 +39,11 @@ if __name__ == "__main__":
     session.commit()
 
     # ишем факты покупки книг у издателя
-    search_pub_id = int(input()) # вводится id издателя 
-
-    subq = session.query(Publisher).filter(Publisher.id == search_pub_id).subquery()
-    subq_1 = session.query(Book).join(subq).subquery()
-    subq_2 = session.query(Stock).join(subq_1, Stock.id_book == subq_1.c.id).subquery()
-    subq_3 = session.query(Shop).join(subq_2, Shop.id == subq_2.c.id_shop).subquery()
-    search_res = session.query(Sale, subq_1.c.title, subq_3.c.name).join(subq_2, Sale.id_stock == subq_2.c.id).join(subq_1). join(subq_3)
+  
+    q = input('Введите id или название издателя: ')
+    if q.isdigit():
+        get_sales(session=session, author_id=int(q))
+    else:
+        get_sales(session=session, publisher_name=q)
     
-    for s in search_res:
-        print(f'{s[1]} | {s[2]} | {s[0]}')
-
     session.close()
--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class Publisher(Base):
     __tablename__ = "publisher"
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=40), unique=True, nullable=False)
+    book = relationship("Book", backref="book")
     def __str__(self):
         return f'Publisher {self.id}: {self.name}'
 
@@ -16,6 +17,7 @@ class Stock(Base):
     id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
     id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
+    sale = relationship("Sale", backref="sale")
     def __str__(self):
         return f'Stock {self.id}: {self.id_book}, {self.id_shop}, {self.count}'
 
@@ -23,8 +25,7 @@ class Book(Base):
     __tablename__ = "book"
     id = sq.Column(sq.Integer, primary_key=True)
     title = sq.Column(sq.String(length=250), nullable=False)
-    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
-    publisher = relationship("Publisher", backref="book")
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)    
     shop = relationship("Stock", backref="book")
     def __str__(self):
         return f'Book {self.id}: {self.title}'
@@ -43,11 +44,24 @@ class Sale(Base):
     price = sq.Column(sq.Numeric(4, 2), nullable=False)
     date_sale = sq.Column(sq.Date, nullable=False)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
-    count = sq.Column(sq.Integer, nullable=False)
-    stock = relationship("Stock", backref="sale")
+    count = sq.Column(sq.Integer, nullable=False)    
     def __str__(self):
         return f'{self.price} | {self.date_sale}'
 
 def create_tables(engine):
     Base.metadata.drop_all(engine) # удаляет данные из базы данных
     Base.metadata.create_all(engine) # создает таблицы
+
+def get_sales(session, author_id = 0, publisher_name = ''):
+    if author_id != 0:
+        res = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).\
+            join(Publisher).join(Stock).join(Sale).join(Shop).\
+            filter(Publisher.id==author_id)
+    else:
+        res = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).\
+            join(Publisher).join(Stock).join(Sale).join(Shop).\
+            filter(Publisher.name.ilike(f'%{publisher_name}%'))
+    
+
+    for book, shop, price, count, date in res:
+        print(f'{book: <40} | {shop: <10} | {price*count: <8} | {date.strftime("%d-%m-%Y")}')
