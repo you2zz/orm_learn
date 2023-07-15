aee9c92e8d347156318a71dbd1f597cd75a6a5df
you2zz <yri_vla@mail.ru>
2023-07-15 18:29:41 +0500

    15072023_1829

--- models.py
+++ models.py
@@ -11,31 +11,31 @@ class Publisher(Base):
     name = sq.Column(sq.String(length=40), unique=True, nullable=False)
     def __str__(self):
         return f'Publisher {self.id}: {self.name}'
-
-class Stock(Base):
-    __tablename__ = "stock"
-    id = sq.Column(sq.Integer, primary_key=True)
-    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False, ondelete="CASCSDE")
-    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False, ondelete="CASCSDE")
-    count = sq.Column(sq.Integer, nullable=False)
-    def __str__(self):
-        return f'Stock {self.id}: {self.id_book}, {self.id_shop}, {self.count}'
-
+    
 class Book(Base):
     __tablename__ = "book"
     id = sq.Column(sq.Integer, primary_key=True)
     title = sq.Column(sq.String(length=250), nullable=False)
-    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False, ondelete="CASCSDE")    
-    shop = relationship('Shop', secondary='stock', back_populates='stock_book')
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)    
+    shop = relationship('Stock', backref='book')
     author = relationship("Publisher", backref="book")
     def __str__(self):
-        return f'Book {self.id}: {self.title}'
+        return f'Book {self.id}: {self.title}'    
+
+class Stock(Base):
+    __tablename__ = "stock"
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+    def __str__(self):
+        return f'Stock {self.id}: {self.id_book}, {self.id_shop}, {self.count}'
     
 class Shop(Base):
     __tablename__ = "shop"
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=100), unique=True, nullable=False)
-    book = relationship("Book", secondary='stock', back_populates='stock_shop')
+    book = relationship("Stock", backref='shop')
     def __str__(self):
         return f'Shop {self.id}: {self.name}'
     
@@ -54,24 +54,24 @@ def create_tables(engine):
     Base.metadata.drop_all(engine) # удаляет данные из базы данных
     Base.metadata.create_all(engine) # создает таблицы
 
-def get_sales(session, author_id = 0, publisher_name = ''):
-    res = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).\
-          join(Publisher).join(Stock).join(Sale).join(Shop).\
-          filter(or_(Publisher.id==author_id, Publisher.name.ilike(f'%{publisher_name}%')))
-
-    for book, shop, price, count, date in res:
-        print(f'{book: <40} | {shop: <10} | {price*count: <8} | {date.strftime("%d-%m-%Y")}')
-
-
 # def get_sales(session, author_id = 0, publisher_name = ''):
-#     if author_id != 0:
-#         res = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).\
-#             join(Publisher).join(Stock).join(Sale).join(Shop).\
-#             filter(Publisher.id==author_id)
-#     else:
-#         res = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).\
-#             join(Publisher).join(Stock).join(Sale).join(Shop).\
-#             filter(Publisher.name.ilike(f'%{publisher_name}%'))
+#     res = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).\
+#           join(Publisher).join(Stock).join(Sale).join(Shop).\
+#           filter(or_(Publisher.id==author_id, Publisher.name.ilike(f'%{publisher_name}%')))
+
+#     for book, shop, price, count, date in res:
+#         print(f'{book: <40} | {shop: <10} | {price*count: <8} | {date.strftime("%d-%m-%Y")}')
+
+
+def get_sales(session, author_id = 0, publisher_name = ''):
+    if author_id != 0:
+        res = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).\
+            join(Publisher).join(Stock).join(Sale).join(Shop).\
+            filter(Publisher.id==author_id)
+    else:
+        res = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).\
+            join(Publisher).join(Stock).join(Sale).join(Shop).\
+            filter(Publisher.name.ilike(f'%{publisher_name}%'))
     
 
     for book, shop, price, count, date in res:
